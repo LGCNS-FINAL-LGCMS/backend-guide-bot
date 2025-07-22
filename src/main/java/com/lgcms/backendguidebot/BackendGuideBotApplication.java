94ea4bd1bdeae42628db49c9dd176c10a2681a62
Sunhokim2 <mcwright@naver.com>
2025-07-22 12:51:36 +0900

    initial commit

---
+++ src/main/java/com/lgcms/backendguidebot/BackendGuideBotApplication.java
@@ -0,0 +1,13 @@
+package com.lgcms.backendguidebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGuideBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGuideBotApplication.class, args);
+    }
+
+}
