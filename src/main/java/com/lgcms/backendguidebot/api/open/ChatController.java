1111247195a298b7165a4f973be3242dc06edf87
Sunhokim2 <mcwright@naver.com>
2025-07-27 09:53:00 +0900

    [LB-135] feat: 질문기반 retriever검색
    질문 쿼리 기반으로 pgvector검색 후 그 컨텍스트 기반으로 답변 생성

--- src/main/java/com/lgcms/backendguidebot/api/open/ChatController.java
+++ src/main/java/com/lgcms/backendguidebot/api/open/ChatController.java
@@ -4,7 +4,12 @@ import com.lgcms.backendguidebot.common.dto.BaseResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
@@ -14,14 +19,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @Slf4j
 @RequestMapping("/open/guide/")
 public class ChatController {
     private final ChatClient.Builder chatClientBuilder;
+    private final VectorStore vectorStore;
 
-    public ChatController(ChatClient.Builder chatClientBuilder) {
+
+    public ChatController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
         this.chatClientBuilder = chatClientBuilder;
+        this.vectorStore = vectorStore;
     }
 
 
@@ -30,8 +41,15 @@ public class ChatController {
             @RequestBody ChatRequest chatRequest
     ) {
         ChatClient chatClient = chatClientBuilder.build();
+        List<Document> Documents = vectorStore.similaritySearch(chatRequest.query);
+        String context = Documents.stream()
+                .map(Document::getText)
+                .collect(Collectors.joining(" "));
 
-        String answer = chatClient.prompt(chatRequest.query)
+        Message systemMessage = new SystemMessage("당신은 친절한 도우미입니다. 이 컨텍스트를 참고해 답변하세요 context : " + context);
+        UserMessage userMessage = new UserMessage(chatRequest.query);
+
+        String answer = chatClient.prompt(new Prompt(List.of(systemMessage, userMessage)))
                 .call().content();
         return ResponseEntity.ok(BaseResponse.ok(new ChatResponse(answer)));
     }
