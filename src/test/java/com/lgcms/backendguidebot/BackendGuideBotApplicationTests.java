f61612681205e5f3e9c6a79d184b426834da811f
Sunhokim2 <mcwright@naver.com>
2025-08-08 13:47:40 +0900

    [LB-37] fix: 배포환경을 위한 fix

--- src/test/java/com/lgcms/backendguidebot/BackendGuideBotApplicationTests.java
+++ src/test/java/com/lgcms/backendguidebot/BackendGuideBotApplicationTests.java
@@ -3,8 +3,9 @@ package com.lgcms.backendguidebot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendGuideBotApplicationTests {
     @Test
