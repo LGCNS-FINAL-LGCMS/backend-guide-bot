3dc455474350796bf08b10e6ffab8e642114a536
Sunhokim2 <mcwright@naver.com>
2025-09-05 13:35:35 +0900

    [LB-449] feat: llm호출 토큰수계산

--- src/main/java/com/lgcms/backendguidebot/common/aspect/TokenMetricsAspect.java
+++ src/main/java/com/lgcms/backendguidebot/common/aspect/TokenMetricsAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.ai.bedrock.titan.api.TitanEmbeddingBedrockApi;
+import org.springframework.ai.chat.metadata.Usage;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -15,6 +17,18 @@ import org.springframework.stereotype.Component;
 public class TokenMetricsAspect {
     private final MeterRegistry meterRegistry;
 
+    @AfterReturning(value = "execution(* org.springframework.ai.chat.model.ChatModel.call(..))", returning = "result")
+    public void recordTokenMetrics(JoinPoint joinPoint, Object result) {
+        if(result instanceof ChatResponse chatResponse) {
+            Usage usage = chatResponse.getMetadata().getUsage();
+            Integer totalTokens = usage.getTotalTokens();
+
+            Counter.builder("ai.tokens")
+                    .description("LLM token usage")
+                    .register(meterRegistry)
+                    .increment(totalTokens);
+        }
+    }
 
     @AfterReturning(value = "execution(* org.springframework.ai.bedrock.titan.api.TitanEmbeddingBedrockApi.embedding(..))", returning = "result")
     public void recordEmbeddingTokenMetrics(JoinPoint joinPoint, Object result) {
